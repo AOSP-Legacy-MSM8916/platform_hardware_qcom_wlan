54661bdcd036c941d3a46d51368230c0af63772b
Harshit Jain <harshitjain6751@gmail.com>
2018-08-30 16:30:54 +0530

    wlan: fix guard makefile

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifneq ($(TARGET_QCOM_WLAN_VARIANT),caf)
+ifneq ($(TARGET_QCOM_WLAN_VARIANT),wlan-caf)
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
 ifneq ($(BOARD_IS_AUTOMOTIVE),true)
 include $(call all-subdir-makefiles)
